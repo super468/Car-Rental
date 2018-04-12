214519b5348af5d1b8fbd2f742d70cd7123339b3
yangtaohua <huayangtao@163.com>
2018-04-12 18:45:00 -0500

    filter html part

--- client/src/app/filter/filter.component.ts
+++ client/src/app/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-filter',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  psgervalue: number;//passengers number
+
+  pricelist: any[] = [];
+
+  options: any[] = []; // object: {id, text} or array: []
+
   constructor() { }
 
   ngOnInit() {
+
+    this.psgervalue=7;
+    this.options = [
+      {id: 'eco', text: 'Economy'},
+      {id: 'cmpt', text: 'Compact'},
+      {id: 'std', text: 'Standard'},
+      {id: 'suv', text: 'SUV'},
+      {id: 'lux', text: 'Luxury'}
+    ];
+    this.pricelist = [
+      {id: '0', text: '--ALL--'},
+      {id: '1', text: 'Under 50$'},
+      {id: '2', text: '50$ - 100'},
+      {id: '3', text: '100$ - 150$'},
+      {id: '4', text: '150$ - 200$'},
+      {id: '5', text: 'Above 200$'}
+      ];
+
+    this.options.forEach(x => x.state = true);
   }
+
+  checkAll(ev) {
+    this.options.forEach(x => x.state = ev.target.checked)
+  }
+
+  isAllChecked() {
+    return this.options.every(_ => _.state);
+  }
+
+
 
 }
