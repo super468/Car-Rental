22886b80a1de725356aaf45a22eb9a4839554c20
super468 <wtsbuct2013@163.com>
2018-04-19 19:31:03 -0500

    add unqiue constraint on favoritelist

--- server/api/models/favoritelist.js
+++ server/api/models/favoritelist.js
@@ -5,4 +5,6 @@ var favoritelistSchema = new mongoose.Schema({
     email:String,
     carid:String
 });
+
+favoritelistSchema.index({email:1,carid:1},{unqiue:true});
 mongoose.model('Favoritelist', favoritelistSchema);
